725956327619c519069a0629e55d7f42e1c733a1
Michael <micbr@pm.me>
2023-06-24 22:23:25 +0200

    First commit

---
+++ bubble_sort.py
@@ -0,0 +1,13 @@
+def bubble_sort(arr, count=0):
+    # Outer "for" loop
+    for i in range(len(arr)):
+        count+=1
+        # Inner "for" loop
+        for j in range(len(arr)-1):
+            count+=1
+            # -1 above because we go +1 below
+            if arr[j] > arr[j+1]:
+                # Swap them
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+        print(arr)
+    return arr, count
